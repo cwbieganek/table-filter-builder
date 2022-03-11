50889feb16f5a11ce5626ccd849aec2ee69d7f3b
Chris Bieganek <cwbieganek@gmail.com>
2022-03-11 08:37:32 -0600

    Add comparison operator HTMLSelect and add "" as possible ComparisonOperator and LogicalOperator values.

--- components/FilterBuilder/FilterBuilder.tsx
+++ components/FilterBuilder/FilterBuilder.tsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 
 import { HTMLSelect } from '@blueprintjs/core';
 
-export type LogicalOperator = "AND" | "OR";
-export type ComparisonOperator = ">" | ">=" | "=" | "<=" | "<";
+export type LogicalOperator = "AND" | "OR" | "";
+export type ComparisonOperator = ">" | ">=" | "=" | "<=" | "<" | "";
+const LOGICAL_OPERATORS: LogicalOperator[] = ["AND", "OR"];
+const COMPARISON_OPERATORS: ComparisonOperator[] = [">", ">=", "=", "<=", "<"]; 
 
 export interface IField {
 	name: string;
@@ -18,20 +20,33 @@ export interface IProps extends React.HTMLProps<HTMLDivElement> {
 const Greeting: React.FC<IProps> = ({ fields }) => {
 	let [ selectedFieldName, setSelectedFieldName ] = useState("");
 
-	function onChange(event: React.ChangeEvent<HTMLSelectElement>) {
+	function onFieldNameSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
 		setSelectedFieldName(event.currentTarget.value);
+	}
+
+	let [ selectedComparisonOperator, setSelectedComparisonOperator ] = useState("");
+
+	function onComparisonOperatorSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
+		setSelectedComparisonOperator(event.currentTarget.value);
 	}
 
 	return (
 		<div>
 			<h2>Filter Builder</h2>
-			<HTMLSelect onChange={onChange}>
+			<HTMLSelect onChange={onFieldNameSelectChange}>
 				<option selected>Choose a field...</option>
 				{fields.map((field, i) => {
 					return <option key={i} value={field.name}>{field.name}</option>;
 				})}
   		</HTMLSelect>
 			<div>Selected field name: {selectedFieldName}</div>
+			<HTMLSelect onChange={onComparisonOperatorSelectChange}>
+				<option selected>Choose a comparison...</option>
+				{COMPARISON_OPERATORS.map((comparisonOperator, i) => {
+					return <option key={i} value={comparisonOperator}>{comparisonOperator}</option>;
+				})}
+  		</HTMLSelect>
+			<div>Selected comparison: {selectedComparisonOperator}</div>
 		</div>
 	);
 };
