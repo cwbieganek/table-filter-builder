0219cc0252f4befa82378948e7babcd430a4bd10
Chris Bieganek <cwbieganek@gmail.com>
2022-03-11 08:48:43 -0600

    Add JSDoc comments

--- components/FilterBuilder/FilterBuilder.tsx
+++ components/FilterBuilder/FilterBuilder.tsx
@@ -3,17 +3,39 @@ import { useState } from 'react';
 
 import { HTMLSelect } from '@blueprintjs/core';
 
+/**
+ * Binary logical operators such as AND, OR, etc.
+ */
 export type LogicalOperator = "AND" | "OR" | "";
+
+/**
+ * Comparison operators such as >, =, <, etc.
+ */
 export type ComparisonOperator = ">" | ">=" | "=" | "<=" | "<" | "";
+
+// These will be used to populate the HTMLSelect components
 const LOGICAL_OPERATORS: LogicalOperator[] = ["AND", "OR"];
 const COMPARISON_OPERATORS: ComparisonOperator[] = [">", ">=", "=", "<=", "<"]; 
 
+/**
+ * Represents a field/column in a table.
+ */
 export interface IField {
+	/**
+	 * The name of the column.
+	 */
 	name: string;
+
+	/**
+	 * The type of the column (text, number, etc.).
+	 */
 	type: "TEXT" | "NUMBER";
 }
 
 export interface IProps extends React.HTMLProps<HTMLDivElement> {
+	/**
+	 * A list of the fields that can be used in the FilterBuilder.
+	 */
 	fields: IField[];
 }
 
