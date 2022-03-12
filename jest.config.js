e1d7b2a96897f0b8424b0c5b2d20f37610fc235c
Chris Bieganek <cwbieganek@gmail.com>
2022-03-12 13:15:22 -0600

    Add modules directory to moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,8 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+
+    '^@/modules/(.*)$': '<rootDir>/modules/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
