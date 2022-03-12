9a6ecf36c7311c30c1fcc33828e0b86953f84706
Chris Bieganek <cwbieganek@gmail.com>
2022-03-12 12:41:28 -0600

    Import ComparisonOperator and change type of operator property to ComparisonOperator

--- modules/Filter/FilterExpression.ts
+++ modules/Filter/FilterExpression.ts
@@ -1,11 +1,13 @@
+import { ComparisonOperator } from "./Logic";
+
 export type ComparableType = string | number | Date;
 
 export default class FilterExpression {
   left: ComparableType;
   right: ComparableType;
-  operator: string;
+  operator: ComparisonOperator;
 
-  constructor(left: ComparableType, right: ComparableType, operator: string) {
+  constructor(left: ComparableType, right: ComparableType, operator: ComparisonOperator) {
     this.left = left;
     this.right = right;
     this.operator = operator;
