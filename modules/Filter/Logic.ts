e82a4a07353169ac07688c61b7a6dd052e1ca5bf
Chris Bieganek <cwbieganek@gmail.com>
2022-03-12 12:38:22 -0600

    Fix more whitespace mess

--- modules/Filter/Logic.ts
+++ modules/Filter/Logic.ts
@@ -1,19 +1,19 @@
 /**
  * Binary logical operators such as AND, OR, etc.
  */
- export type LogicalOperator = "AND" | "OR" | "";
+export type LogicalOperator = "AND" | "OR" | "";
 
 /**
  * Comparison operators such as >, =, <, etc.
  */
- export type ComparisonOperator = ">" | ">=" | "=" | "<=" | "<" | "";
+export type ComparisonOperator = ">" | ">=" | "=" | "<=" | "<" | "";
  
  /**
  * Array of binary logical operators such as AND, OR, etc.
  */
- export const LOGICAL_OPERATORS: LogicalOperator[] = ["AND", "OR"];
+export const LOGICAL_OPERATORS: LogicalOperator[] = ["AND", "OR"];
 
 /**
 * Array of comparison operators such as >, =, <, etc.
 */
- export const COMPARISON_OPERATORS: ComparisonOperator[] = [">", ">=", "=", "<=", "<"];
+export const COMPARISON_OPERATORS: ComparisonOperator[] = [">", ">=", "=", "<=", "<"];
