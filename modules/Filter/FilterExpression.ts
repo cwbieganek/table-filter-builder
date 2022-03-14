2e93672a97a8c088d9ce2e07cffd8ac34f4472ea
Chris Bieganek <cwbieganek@gmail.com>
2022-03-13 19:45:54 -0500

    Make properties private and add _ to their names

--- modules/Filter/FilterExpression.ts
+++ modules/Filter/FilterExpression.ts
@@ -12,43 +12,43 @@ export default class FilterExpression {
   /**
    * The value on the left side of the expression. Will always be the value of a specific field.
    */
-  left: ComparableType;
+  private _left: ComparableType;
 
   /**
    * The value on the right side of the expression that a field value will be compared to.
    */
-  right: ComparableType;
+  private _right: ComparableType;
 
   /**
    * The comparison operator that will be used to compare the left and right values, such as >, <, or =.
    */
-  operator: ComparisonOperator;
+  private _operator: ComparisonOperator;
 
   constructor(left: ComparableType, right: ComparableType, operator: ComparisonOperator) {
-    this.left = left;
-    this.right = right;
-    this.operator = operator;
+    this._left = left;
+    this._right = right;
+    this._operator = operator;
   }
 
   /**
    * Evaluates the expression and returns a boolean.
    */
   public evaluate(): boolean {
-    switch (this.operator) {
+    switch (this._operator) {
       case "<":
-        return this.left < this.right;
+        return this._left < this._right;
       case "<=":
-        return this.left <= this.right;
+        return this._left <= this._right;
       case "=":
-        return this.left === this.right;
+        return this._left === this._right;
       case ">":
-        return this.left > this.right;
+        return this._left > this._right;
       case ">=":
-          return this.left >= this.right;
+          return this._left >= this._right;
       case "!=":
-        return this.left != this.right;
+        return this._left != this._right;
       default:
-        throw new ComparisonError(`${this.operator} is not valid a operator. Must be a ComparisonOperator type.`);
+        throw new ComparisonError(`${this._operator} is not valid a operator. Must be a ComparisonOperator type.`);
     }
   }
 }
