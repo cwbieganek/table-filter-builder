baa964cbee7ea26c88e418c3a90f18eb2c65e90b
Chris Bieganek <cwbieganek@gmail.com>
2022-03-21 18:46:58 -0500

    Initial commit

---
+++ modules/Fake.ts
@@ -0,0 +1,103 @@
+type FakeRecord = {
+	firstName: string;
+	lastInitial: string;
+	age: number;
+	jobTitle: string;
+	salary: number;
+	tenure: number;
+};
+
+const FIRST_NAMES = [
+	"Chris",
+	"Jane",
+	"John",
+	"Jodi",
+	"Virginia"
+];
+
+const LAST_INITIALS = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E"
+];
+
+const JOB_TITLES = [
+	"QA Analyst",
+	"Software Developer",
+	"Software Architect",
+	"DevOps Engineer"
+];
+
+function getFakeName(): string {
+	let randomIndex = getRandomInt(0, 4);
+
+	return FIRST_NAMES[randomIndex];
+}
+
+function getFakeLastInitial(): string {
+	let randomIndex = getRandomInt(0, 4);
+
+	return LAST_INITIALS[randomIndex];
+}
+
+function getFakeJobTitle(): string {
+	let randomIndex = getRandomInt(0, 3);
+
+	return JOB_TITLES[randomIndex];
+}
+
+function getFakeAge(): number {
+	return getRandomInt(18, 100);
+}
+
+function getFakeSalary(): number {
+	return getRandomInt(50, 200) * 1000;
+}
+
+function getFakeTenure(): number {
+	return getRandomInt(1, 30);
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function getFakeRecord(): FakeRecord {
+	let fakeRecord: FakeRecord = {
+		firstName: getFakeName(),
+		lastInitial: getFakeLastInitial(),
+		age: getFakeAge(),
+		jobTitle: getFakeJobTitle(),
+		salary: getFakeSalary(),
+		tenure: getFakeTenure()
+	}
+
+	return fakeRecord;
+}
+
+/**
+ * Resolves to an array of 100 FakeRecord objects. Adds an artificial timeout to simulate
+ * requesting the data from an API.
+ */
+export function getFakeData(): Promise<FakeRecord[]> {
+	return new Promise((resolve, reject) => {
+		let fakeRecords: FakeRecord[] = [];
+
+		for (let i = 1; i <= 100; i++) {
+			// Get fake record and add it to the list
+			let fakeRecord = getFakeRecord();
+			fakeRecords.push(fakeRecord);
+		}
+
+		// Wait half a second before resolving
+		setTimeout(() => {
+			resolve(fakeRecords);
+		}, 200);
+	});
+}
