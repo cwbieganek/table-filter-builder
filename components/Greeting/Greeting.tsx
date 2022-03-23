11c7c7e7c23db9f09fc6f7356d68855d70eac34c
Chris Bieganek <cwbieganek@gmail.com>
2022-03-23 07:39:31 -0500

    Center text in greeting

--- components/Greeting/Greeting.tsx
+++ components/Greeting/Greeting.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styles from './Greeting.module.css';
+
 export interface Props extends React.HTMLProps<HTMLDivElement> {
 	firstName: string;
 	lastName: string;
@@ -8,7 +10,7 @@ export interface Props extends React.HTMLProps<HTMLDivElement> {
 const Greeting: React.FC<Props> = ({ firstName, lastName }) => {
 	return (
 		<div>
-			<h1>Hello {firstName} {lastName}!</h1>
+			<h1 className={styles.h1}>Hello {firstName} {lastName}!</h1>
 		</div>
 	);
 };
