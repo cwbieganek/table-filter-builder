88e784a80684dd520f455cf14ca15e9df82be9e0
Chris Bieganek <cwbieganek@gmail.com>
2022-03-23 07:19:35 -0500

    Update Table component

--- components/TableExample/TableExample.tsx
+++ components/TableExample/TableExample.tsx
@@ -1,33 +1,55 @@
 import { HotkeysProvider } from "@blueprintjs/core";
-import { Column, Table2 } from "@blueprintjs/table";
-import { FakeRecord, getFakeData } from "@/modules/Fake";
-import React from "react";
+import { Cell, CellRenderer, Column, ColumnLoadingOption, ICellProps, Table2 } from "@blueprintjs/table";
+import { FakeRecord, getFake2dArray } from "@/modules/Fake";
+import { ComparableType } from "@/modules/Logic/LogicalExpression";
+import React, { ReactElement } from "react";
 import { useState, useEffect } from "react";
+import { render } from "@testing-library/react";
 
 const TableExample: React.FC<{}> = () => {
-	const [fakeRecords, setFakeRecords] = useState<FakeRecord[]>([]);
+	const [fakeRows, setFakeRows] = useState<ComparableType[][]>([]);
+	const [loadingData, setLoadingData] = useState<boolean>(true);
 
 	// Get fake data asynchronously, but only once
 	useEffect(() => {
-		getFakeData().then((fakeData) => {
+		getFake2dArray().then((fakeRows) => {
 			console.log("Got fake data:");
-			console.log(fakeData);
-			setFakeRecords(fakeData);
+			console.log(fakeRows);
+			setFakeRows(fakeRows);
+			setLoadingData(false);
 		});
 	}, []);
 
-	return(
-		<HotkeysProvider>
+	function getCellData(fakeRows: ComparableType[][], rowIndex: number, columnIndex: number): ComparableType {
+		return fakeRows[rowIndex][columnIndex];
+	}
+	
+	function cellRenderer (rowIndex: number, columnIndex: number): ReactElement<ICellProps> {
+		return (<Cell>{getCellData(fakeRows, rowIndex, columnIndex)}</Cell>);
+	}
+
+	function renderLoading() {
+		return(
+			<h2>Loading data...</h2>
+		);
+	}
+
+	function renderTable() {
+		return(
+			<HotkeysProvider>
 				<Table2 numRows={100}>
-						<Column name="First Name" />
-						<Column name="Last Initial" />
-						<Column name="Age" />
-						<Column name="Job Title" />
-						<Column name="Salary" />
-						<Column name="Tenure" />
+						<Column name="First Name" cellRenderer={cellRenderer} />
+						<Column name="Last Initial" cellRenderer={cellRenderer} />
+						<Column name="Age" cellRenderer={cellRenderer} />
+						<Column name="Job Title" cellRenderer={cellRenderer} />
+						<Column name="Salary" cellRenderer={cellRenderer} />
+						<Column name="Tenure" cellRenderer={cellRenderer} />
 				</Table2>
-		</HotkeysProvider>
-	);
+			</HotkeysProvider>
+		);
+	}
+
+	return(loadingData ? renderLoading() : renderTable());
 };
 
 export default TableExample;
