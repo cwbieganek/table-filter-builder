8eabaada311d9f33775ed886a48e00275e8bf420
Chris Bieganek <cwbieganek@gmail.com>
2022-03-26 13:49:35 -0500

    Use DarkThemeProvider

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -6,24 +6,29 @@ import { Alignment, Navbar, NavbarGroup, NavbarHeading } from "@blueprintjs/core
 // Blueprint CSS
 import { Classes } from "@blueprintjs/core";
 
+// Dark theme provider
+import DarkThemeProvider from '../DarkThemeProvider/DarkThemeProvider';
+
 // Custom CSS
 import styles from './Layout.module.css';
 
 const TableExample: React.FC<{}> = ({ children }) => {
 	return(
-		<div className={`${Classes.DARK} ${styles.container}`}>
-			<header className={styles.header}>
-				<Navbar>
-					<NavbarGroup align={Alignment.CENTER}>
-						<NavbarHeading>Table Filter Builder</NavbarHeading>
-					</NavbarGroup>
-				</Navbar>
-			</header>
-			<main>
-				{children}
-			</main>
-			<footer>Footer</footer>
-		</div>
+		<DarkThemeProvider>
+			<div className={styles.container}>
+				<header className={styles.header}>
+					<Navbar>
+						<NavbarGroup align={Alignment.CENTER}>
+							<NavbarHeading>Table Filter Builder</NavbarHeading>
+						</NavbarGroup>
+					</Navbar>
+				</header>
+				<main>
+					{children}
+				</main>
+				<footer>Footer</footer>
+			</div>
+		</DarkThemeProvider>
 	);
 };
 
