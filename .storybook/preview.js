393a6d1936864d613f15a53aee6fc2dcbddbb4a2
Chris Bieganek <cwbieganek@gmail.com>
2022-03-26 11:47:58 -0500

    Import Blueprint CSS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import '../node_modules/normalize.css';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+import '../node_modules/@blueprintjs/table/lib/css/table.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
