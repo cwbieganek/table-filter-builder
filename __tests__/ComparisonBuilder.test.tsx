09815eff5641eb219a977fac844c5f0a0e149505
Chris Bieganek <cwbieganek@gmail.com>
2022-03-25 21:51:50 -0500

    Add test of ComparisonBuilder

---
+++ __tests__/ComparisonBuilder.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import ComparisonBuilder, { IField } from '@/components/ComparisonBuilder/ComparisonBuilder';
+
+describe('Greeting', () => {
+	it('renders without crashing', () => {
+		// Test data
+		let fields: IField[] = [
+			{
+				name: "Title",
+				type: "TEXT"
+			},
+			{
+				name: "Salary",
+				type: "NUMBER"
+			},
+			{
+				name: "Age",
+				type: "NUMBER"
+			}
+		];
+
+		render(<ComparisonBuilder title="Comparison 42" fields={fields} />);
+	});
+});
