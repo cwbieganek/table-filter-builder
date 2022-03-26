7595ca0c6191d236ad49e9da9b38517b8a0ed33c
Chris Bieganek <cwbieganek@gmail.com>
2022-03-26 11:13:02 -0500

    Specify Alignment enum member values

--- components/Greeting/Greeting.tsx
+++ components/Greeting/Greeting.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 
 import styles from './Greeting.module.css';
 
+export enum Alignment {
+	LEFT = "LEFT",
+	CENTER = "CENTER",
+	RIGHT = "RIGHT"
+}
+
 export interface Props extends React.HTMLProps<HTMLDivElement> {
 	firstName: string;
 	lastName: string;
+	alignment?: Alignment;
 }
 
-const Greeting: React.FC<Props> = ({ firstName, lastName }) => {
+const Greeting: React.FC<Props> = ({ firstName, lastName, alignment }) => {
+	let className = styles.h1;
+
+	if (alignment) {
+		switch (alignment) {
+			case Alignment.LEFT:
+				break;
+			case Alignment.CENTER:
+				className += styles.centerText;
+				break;
+			case Alignment.RIGHT:
+				className += styles.rightText;
+				break;
+		}
+	}
+
 	return (
 		<div>
-			<h1 className={styles.h1}>Hello {firstName} {lastName}!</h1>
+			<h1 className={className}>Hello {firstName} {lastName}!</h1>
 		</div>
 	);
 };
